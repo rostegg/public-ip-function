c72a5e52d0a0c7e4ab13ebcf7e648ccbef1a1120
rostegg <rostik.nikolaenko@gmail.com>
2020-08-03 22:18:18 +0300

    💾

---
+++ functions/index.js
@@ -0,0 +1,12 @@
+const functions = require('firebase-functions');
+const util = require('util');
+
+exports.getPublicIp = functions.https.onRequest(async (req, res) => {
+    const ipAddress =
+        req.headers['x-appengine-user-ip'] || req.headers['x-forwarded-for'] || req.headers['fastly-client-ip'];
+    const countryCode = req.headers['x-appengine-country'];
+    res.json({
+        ip: ipAddress,
+        countryCode: countryCode,
+    });
+});
